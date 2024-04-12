3f22d95dc5f4053402395dcefd5313d5c3d0d6c1
Tseng-Wei Lee <mike007tw@gmail.com>
2024-04-13 01:18:08 +0200

    ADD: new file

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 # This is a sample Python script.
 import numpy as np
 import pandas
+
 import scipy
-import torch
+
 
 def main():
-    # Use a breakpoint in the code line below to debug your script.
-    print("Hello")  # Press Ctrl+F8 to toggle the breakpoint.
+
+    print("Hello")
     print("New line")
 
 
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     main()
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
